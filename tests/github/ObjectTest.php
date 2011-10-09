8c7e55beb1204d4491d7a51e8741dab82649cd38
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-10-09 13:57:59 +0100

    Test lazy loading and getter behaviour

--- tests/github/ObjectTest.php
+++ tests/github/ObjectTest.php
@@ -14,17 +14,21 @@
  */
 class Github_ObjectTest extends Unittest_TestCase
 {
-	protected $github = null;
+    /**
+	 *
+	 * @var PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $mock_github = null;
 	
 	public function setUp()
 	{
 		parent::setUp();
-		$this->github = $this->getMock('Github', array('api'));	
+		$this->mock_github = $this->getMock('Github', array('api', 'api_json'));	
 	}
 
 	public function test_unknown_fields_raise_exception()
 	{				
-		$foo = new Github_Object_Foo($this->github, array());
+		$foo = new Github_Object_Foo($this->mock_github, array());
 		
 		try
 		{
@@ -40,6 +44,53 @@ class Github_ObjectTest extends Unittest_TestCase
 		
 		$this->fail('Expected Github_Exception_InvalidProperty was not thrown');
 	}
+	
+	public function test_known_fields_are_returned()
+	{
+		$foo = new Github_Object_Foo($this->mock_github, array('field_1'=>'bar'));
+		$this->assertEquals('bar',$foo->field_1);
+	}
+	
+	public function test_object_fields_are_instantiated()
+	{
+		$foo = new Github_Object_Foo($this->mock_github, 
+				array('bar'=>array(
+					'bar_field' => 'foo'
+				)));
+		$this->assertInstanceOf('Github_Object_Bar', $foo->bar);
+	}
+	
+	public function test_loaded_fields_do_not_trigger_loading()
+	{
+		$foo = new Github_Object_Foo($this->mock_github,
+				array('field_1' => 'bar'));
+		
+		$this->mock_github->expects($this->never())
+					 ->method('api_json');
+		$this->mock_github->expects($this->never())
+					->method('api');
+		
+		$test = $foo->field_1;		
+	}
+	
+	public function test_empty_fields_trigger_load()
+	{
+		$foo = new Github_Object_Foo($this->mock_github,
+				array(
+					'url' => 'my/mock/foo'
+				));
+		
+		$this->mock_github->expects($this->once())
+				->method('api_json')
+				->with('my/mock/foo',
+					   'GET')
+				->will($this->returnValue(
+						array('url'=>'my/mock/foo',
+							  'field_1'=>'loaded')));
+		
+		$this->assertEquals('loaded', $foo->field_1);
+	}
+		
 }
 
 
@@ -49,5 +100,12 @@ class Github_Object_Foo extends Github_Object
 		'url' => null,
 		'bar' => 'Github_Object_Bar',
 		'field_1' => null
+	);
+}
+
+class Github_Object_Bar extends Github_Object
+{
+	protected $_fields = array(
+		'bar_field' => null
 	);
 }
