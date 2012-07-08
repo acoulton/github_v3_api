604c876d7c659c2102811670005227082236fd35
Andrew Hutchings <andrew.hutchings@gmail.com>
2012-07-08 14:28:28 -0400

    Add Github_Repo_Git_Tag class

---
+++ classes/github/repo/git/tag.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * <describe the class here>
+ *
+ * @package    GithubAPIv3
+ * @category   Objects
+ * @author     Andrew Coulton
+ * @copyright  (c) 2011 Andrew Coulton
+ * @license    http://kohanaframework.org/license
+ *
+ * @property string $zipball_url
+ * @property string $tarball_url
+ * @property Github_Repo_Git_Commit $commit
+ * @property string $name
+ */
+class Github_Repo_Git_Tag extends Github_Object
+{
+	protected $_fields = array (
+		'zipball_url' => NULL,
+		'tarball_url' => NULL,
+		'commit' => 'Github_Repo_Git_Commit',
+		'name' => NULL,
+	);
+}
